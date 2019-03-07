65bf8f25109d1b40bfb2b1b9b1d2fa5ac3662c1f
Simran Soin <simransoin@Simrans-MacBook-Pro-87.local>
2019-03-07 17:16:47 -0500

    Update disparity map

--- calibrate_error.py
+++ calibrate_error.py
@@ -5,7 +5,7 @@ def calculate_error(image_name1, image_name2, calibrated_name):
     # termination criteria
     criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-    objp = np.zeros((6*9,3), np.float32)
+    objp = np.zeros((np.prod(6*9), 3), np.float32)
     objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
     # Arrays to store object points and image points from all the images.
     objpoints = [] # 3d point in real world space
@@ -15,23 +15,23 @@ def calculate_error(image_name1, image_name2, calibrated_name):
         img = cv.imread(fname)
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         # Find the chess board corners
-        ret, corners = cv.findChessboardCorners(gray, (9,6), None)
+        ret, corners = cv.findChessboardCorners(gray, (9,6), True)
         # If found, add object points, image points (after refining them)
         if ret == True:
             objpoints.append(objp)
             corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-            imgpoints.append(corners)
+            imgpoints.append(corners.reshape(-1,2))
             # Draw and display the corners
             cv.drawChessboardCorners(img, (9,6), corners2, ret)
             cv.imshow('img', img)
             cv.waitKey(500)
     cv.destroyAllWindows()
-
     ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
     img = cv.imread(image_name2)
     h,  w = img.shape[:2]
     newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+    focal_length = (mtx[0][0], mtx[1][1])
     INPUTARRAYMTX2 = mtx
     INPUTARRAYDISTCOEFFS2 = dist
     SIZE = (w,h)
-    return (INPUTARRAYMTX2, INPUTARRAYDISTCOEFFS2, SIZE, objpoints, imgpoints, img)
+    return (INPUTARRAYMTX2, INPUTARRAYDISTCOEFFS2, SIZE, objpoints, imgpoints, focal_length)
