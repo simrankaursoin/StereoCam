67a4b180c5ded92d8769727b11be8e2d0384e960
Simran Soin <simransoin@10-18-141-217.dynapool.wireless.nyu.edu>
2019-03-07 17:58:14 -0500

    Adjust disparity factors

--- disparity_map.py
+++ disparity_map.py
@@ -3,8 +3,8 @@ import cv2
 import sys
 import matplotlib.pyplot as plt
 from calibrate_error import calculate_error
-INPUTARRAYMTX_left, INPUTARRAYDISTCOEFFS_left, SIZE_L, objpoints_l, imgpoints_l, focal_length_left = calculate_error("left2.png", "left.png", "calibrated_left.png")
-INPUTARRAYMTX_right, INPUTARRAYDISTCOEFFS_right, SIZE_R,objpoints_r, imgpoints_r, focal_length_right = calculate_error("right2.png", "right.png", "calibrated_right.png")
+INPUTARRAYMTX_left, INPUTARRAYDISTCOEFFS_left, SIZE_L, objpoints_l, imgpoints_l, focal_length_left = calculate_error("left.png", "left2.png", "calibrated_left.png")
+INPUTARRAYMTX_right, INPUTARRAYDISTCOEFFS_right, SIZE_R,objpoints_r, imgpoints_r, focal_length_right = calculate_error("right.png", "right2.png", "calibrated_right.png")
 objpoints = objpoints_l
 TERMINATION_CRITERIA = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 1, 1e-5)
 
@@ -30,14 +30,12 @@ rightMapY = calibration["rightMapY"]
 rightROI = tuple(calibration["rightROI"])
 
 REMAP_INTERPOLATION = cv2.INTER_AREA
-stereoMatcher = cv2.StereoBM_create(160,15)
-stereoMatcher.setPreFilterSize(5)
-stereoMatcher.setPreFilterCap(16)
-stereoMatcher.setMinDisparity(-39)
-stereoMatcher.setBlockSize(21)
+stereoMatcher = cv2.StereoBM_create(96,15)
+
+stereoMatcher.setMinDisparity(-20)
+stereoMatcher.setBlockSize(15)
 stereoMatcher.setSpeckleRange(20)
 stereoMatcher.setSpeckleWindowSize(0)
-stereoMatcher.setTextureThreshold(507)
 stereoMatcher.setUniquenessRatio(0)
 cap0 = cv2.VideoCapture(1)
 cap1 = cv2.VideoCapture(0)
